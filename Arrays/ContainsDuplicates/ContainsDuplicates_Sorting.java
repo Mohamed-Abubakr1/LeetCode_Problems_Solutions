f8515c852f8f4ba9711d280ce9b9e585df5ee59c
Mohamed Abu Bakr <mobakrleetcode@gmail.com>
2026-04-02 01:54:38 +0200

    Create ContainsDuplicates_Sorting.java

---
+++ Arrays/ContainsDuplicates/ContainsDuplicates_Sorting.java
@@ -0,0 +1,15 @@
+/*
+ * Sorting Approach
+ * Time Complexity: O(n log n)
+ * Space Complexity: O(1)
+*/
+
+public boolean containsDuplicate(int[] nums) {
+    Arrays.sort(nums);
+
+    for (int i = 1; i < nums.length; i++) {
+        if (nums[i] == nums[i - 1]) return true;
+    }
+
+    return false;
+}
