a7944af9bbf79b1da247254baa1ed2e523bc7f35
Mohamed Abu Bakr <mobakrleetcode@gmail.com>
2026-04-04 20:51:19 +0200

    Create ReverseLinkedList_Recursive.java

---
+++ Linked-List/Reverse-Linked-List/ReverseLinkedList_Recursive.java
@@ -0,0 +1,16 @@
+/*
+ * Recursive Approach
+ * Time Complexity: O(n)
+ * Space Complexity: O(n) due to the recursion stack
+ */
+
+public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+
+    ListNode newHead = reverseList(head.next);
+
+    head.next.next = head;
+    head.next = null;
+
+    return newHead;
+}
